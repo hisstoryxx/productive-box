9252bd3d01c497d08c982b6310c3fb8481bf51c1
cho <61859083+hisstoryxx@users.noreply.github.com>
2023-01-23 17:56:11 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ interface IRepo {
     files: {
       [filename]: {
         // eslint-disable-next-line quotes
-        filename: (morning + daytime) > (evening + night) ? "I'm an early1 🐤 나는 일찍 일어나는 새" : "I'm a night 🦉 나는 올빼미",
+        filename: (morning + daytime) > (evening + night) ? "I'm an earlyy1 🐤 " : "I'm a night 🦉 나는 올빼미",
         content: lines.join('\n'),
       },
     },
